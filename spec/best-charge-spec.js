67bd42e218113cd89aec9bab9abe98ed3dd2592f
Peng.Li <peng.li@rea-group.com>
2016-07-17 20:29:39 +0800

    add basic code

---
+++ spec/best-charge-spec.js
@@ -0,0 +1,11 @@
+describe('Take out food', function () {
+
+  beforeEach(function () {
+    // do something before each case
+  });
+
+  it('1+1 = 2', function() {
+    expect(1+1).toEqual(2);
+  });
+
+});
