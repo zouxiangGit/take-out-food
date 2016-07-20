ce8c6cc9f2cf1ebadf6620a40f9ffe6a05b9a569
Peng.Li <peng.li@rea-group.com>
2016-07-20 23:46:31 +0800

    add the intergration test

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,11 +1,20 @@
 describe('Take out food', function () {
 
-  beforeEach(function () {
-    // do something before each case
-  });
-
-  it('1+1 = 2', function() {
-    expect(1+1).toEqual(2);
+  it('should generate best charge', function() {
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let summary = bestCharge(inputs);
+    let expected = `
+============= 订餐明细 =============
+黄焖鸡 x 1 = 18元
+肉夹馍 x 2 = 12元
+凉皮 x 1 = 8元
+-----------------------------------
+使用优惠:
+指定菜品半价(黄焖鸡，凉皮)，省13元
+-----------------------------------
+总计：25元
+===================================`.trim()
+    expect(summary).toEqual(expected)
   });
 
 });
